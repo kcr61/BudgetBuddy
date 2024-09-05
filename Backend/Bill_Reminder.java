824fa68713c0fa8e413cd0c1c27ff53e4193c091
HJohn0360 <john.harris-7@selu.edu>
2024-09-05 14:07:14 -0500

    This is to make sure I know how to commit.

--- Backend/Bill_Reminder.java
+++ Backend/Bill_Reminder.java
@@ -3,5 +3,6 @@ package Backend;
 public class Bill_Reminder {
     public static void main(String[] args) {
         System.out.println("Hello Bill Reminder!");
+        System.out.println("This a test message");
     }
 }
