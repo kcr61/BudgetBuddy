1eb0fe11c1ec63d0a36c46db326174cf17dc5e54
kcr61 <rkchelle64@gmail.com>
2024-09-05 18:31:58 -0500

    files-empty-just-setup

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
-
+import { Redirect, router } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, SrcollView, Text, View } from 'react-native';
+import {SafeAreaView} from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello world</Text>
+      <Text s
+      tyle={{ color: '#8aecc7',}}>hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
